2f28f659284f6d509cdc6e65e13ec978ab8ffdd5
Jasmin Chebo <Jasmin.Chebo@gmail.com>
2014-05-26 14:49:08 +0200

    baza

---
+++ sistemZaEvidencijuStalnihSredstava/bazahibernate.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 26, 2014 at 02:45 PM
+-- Server version: 5.5.36
+-- PHP Version: 5.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `bazahibernate`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipstalnogsredstva`
+--
+
+CREATE TABLE IF NOT EXISTS `tipstalnogsredstva` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `NAZIV` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
