c0d8473aaa9d3b15e30827cb350714f87653441b
Dajan <dbrackovic2@etf.unsa.ba>
2014-05-30 12:18:21 +0200

    Izmjena StalnogSredstva

--- sistemZaEvidencijuStalnihSredstava/src/main/java/ba/unsa/etf/si/app/sistemZaEvidencijuStalnihSredstava/IzmjenaStalnogSredstva.java
+++ sistemZaEvidencijuStalnihSredstava/src/main/java/ba/unsa/etf/si/app/sistemZaEvidencijuStalnihSredstava/IzmjenaStalnogSredstva.java
@@ -18,9 +18,17 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.ButtonGroup;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 
+import ba.unsa.etf.si.klase.StalnoSredstvo;
+import ba.unsa.etf.si.klase.TipStalnogSredstva;
 import ba.unsa.etf.si.util.HibernateUtil;
+
+import javax.swing.JComboBox;
+
+import java.awt.Color;
+import java.util.List;
 
 public class IzmjenaStalnogSredstva extends JFrame {
 
@@ -30,10 +38,13 @@ public class IzmjenaStalnogSredstva extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField textNaziv;
+	private JTextField textLokacija;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private Session session;
+	private JComboBox komboTipovi;
+	private JComboBox komboSredstva;
 
 	/**
 	 * Launch the application.
@@ -57,76 +68,75 @@ public class IzmjenaStalnogSredstva extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	public List<TipStalnogSredstva> SviTipovi(){
+		Query query = session.createQuery("from TipStalnogSredstva");
+		List<TipStalnogSredstva> results = (List<TipStalnogSredstva>) query.list();
+		return results;
+	}
+	public List<StalnoSredstvo> SvaSredstva(){
+		Query query = session.createQuery("from StalnoSredstvo");
+		List<StalnoSredstvo> results = (List<StalnoSredstvo>) query.list();
+		return results;
+	}
+	private void popuni(){
+		textNaziv.setText(((StalnoSredstvo)komboSredstva.getSelectedItem()).getNaziv());
+		textLokacija.setText(((StalnoSredstvo)komboSredstva.getSelectedItem()).getLokacija());
+	}
 	public IzmjenaStalnogSredstva(Session sesija) {
 		session = sesija;
 		setTitle("Izmjena stalnog sredstva");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 369, 331);
+		setBounds(100, 100, 367, 234);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JLabel lblNoviNazivStalnog = new JLabel("Novi naziv stalnog sredstva:");
-		lblNoviNazivStalnog.setBounds(37, 41, 160, 14);
+		lblNoviNazivStalnog.setBounds(43, 75, 172, 14);
 		contentPane.add(lblNoviNazivStalnog);
 
 		JLabel lblNoviTipStalnog = new JLabel("Novi tip stalnog sredstva:");
-		lblNoviTipStalnog.setBounds(47, 66, 153, 14);
+		lblNoviTipStalnog.setBounds(56, 100, 153, 14);
 		contentPane.add(lblNoviTipStalnog);
 
 		JLabel lblNovaLokacijaStalnog = new JLabel(
 				"Nova lokacija stalnog sredstva:");
-		lblNovaLokacijaStalnog.setBounds(20, 91, 190, 14);
+		lblNovaLokacijaStalnog.setBounds(28, 125, 179, 14);
 		contentPane.add(lblNovaLokacijaStalnog);
 
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(UIManager
-				.getBorder("TitledBorder.border"), "Amortizacija",
-				TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(47, 139, 261, 92);
-		contentPane.add(panel);
-		panel.setLayout(null);
-
-		JLabel lblDatumKadaSe = new JLabel("Datum kada se amortizuje:");
-		lblDatumKadaSe.setBounds(10, 41, 160, 14);
-		panel.add(lblDatumKadaSe);
-
-		JRadioButton rdbtnGodinje = new JRadioButton("Godi\u0161nje");
-		buttonGroup.add(rdbtnGodinje);
-		rdbtnGodinje.setBounds(163, 24, 109, 23);
-		panel.add(rdbtnGodinje);
-
-		JRadioButton rdbtnMjeseno = new JRadioButton("Mjese\u010Dno");
-		buttonGroup.add(rdbtnMjeseno);
-		rdbtnMjeseno.setBounds(163, 51, 109, 23);
-		panel.add(rdbtnMjeseno);
-
 		textField = new JTextField();
-		textField.setBounds(207, 38, 113, 20);
+		textField.setBounds(207, 72, 134, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
-
-		textField_1 = new JTextField();
-		textField_1.setBounds(207, 63, 113, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		textNaziv = textField;
 
 		textField_2 = new JTextField();
-		textField_2.setBounds(207, 88, 113, 20);
+		textField_2.setBounds(207, 122, 134, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
+		textLokacija = textField_2;
 
-		JButton btnUredu = new JButton("Uredu");
+		JButton btnUredu = new JButton("Izmijeni");
 		btnUredu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane
-						.showMessageDialog(
-								null,
-								"Nije implementirano! \nKlikom na ovo dugme stalno sredstvo �e biti izmijenjeno.");
-			}
+					StalnoSredstvo ss = (StalnoSredstvo)komboSredstva.getSelectedItem();
+					TipStalnogSredstva tip = (TipStalnogSredstva)komboTipovi.getSelectedItem();
+					if(ss.isuUpotrebi()==false){
+						session.getTransaction().begin();
+						ss.setTip(tip);
+						ss.setNaziv(textNaziv.getText());
+						ss.setLokacija(textLokacija.getText());
+						session.update(ss);
+						session.getTransaction().commit();
+						JOptionPane.showMessageDialog(null, "Sredstvo uspješno izmijenjeno.");
+						dispose();
+					}
+					else
+						JOptionPane.showMessageDialog(null, "Izabrali ste sredstvo u upotrebi.");
+				}
 		});
-		btnUredu.setBounds(143, 250, 89, 23);
+		btnUredu.setBounds(153, 165, 89, 23);
 		contentPane.add(btnUredu);
 
 		JButton btnIzai = new JButton("Iza\u0111i");
@@ -135,8 +145,33 @@ public class IzmjenaStalnogSredstva extends JFrame {
 				dispose();
 			}
 		});
-		btnIzai.setBounds(242, 250, 89, 23);
+		btnIzai.setBounds(252, 165, 89, 23);
 		contentPane.add(btnIzai);
+		
+		JComboBox comboBox = new JComboBox(SviTipovi().toArray());
+		comboBox.setBounds(207, 97, 134, 20);
+		contentPane.add(comboBox);
+		komboTipovi = comboBox;
+		
+		JLabel lblNewLabel = new JLabel("Može se mijenjati samo sredstvo koje nije u upotrebi!");
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setBounds(10, 11, 321, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblIzaberiteStalnoSredstvo = new JLabel("Izaberite stalno sredstvo:");
+		lblIzaberiteStalnoSredstvo.setBounds(56, 52, 153, 14);
+		contentPane.add(lblIzaberiteStalnoSredstvo);
+		
+		JComboBox comboBox_1 = new JComboBox(SvaSredstva().toArray());
+		comboBox_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				popuni();
+			}
+		});
+		comboBox_1.setBounds(207, 49, 134, 20);
+		contentPane.add(comboBox_1);
+		komboSredstva = comboBox_1;
+		popuni();
 	}
 
 }
